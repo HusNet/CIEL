d5510adec7b841d4fc780a080285a64936373c5d
Sylvain Roh <syzin12@gmail.com>
2018-05-18 17:43:58 +0200

    Login form and Code Confirmation Form

--- config/routes.js
+++ config/routes.js
@@ -7,30 +7,33 @@ module.exports = function(app, router) {
         });
     });
 
-    //login route
-    let login = require('../app/controllers/login_ctrl');
+
+
+    //home route
+    let home = require('../app/controllers/home_ctrl');
     router.get('/', function(req, res, next) {
-        login.index(req, res, next);
+        home.index(req, res, next);
     });
 
     //login form route
+    let login = require('../app/controllers/login_ctrl');
     router.get('/login', function(req, res, next) {
         login.login(req, res, next);
     });
     router.post('/login', function(req, res, next) {
        login.login_do(req, res, next);
     });
+    router.get('/login/code', function(req, res, next) {
+        login.code(req, res, next);
+    });
+    router.post('/login/code', function(req, res, next) {
+        login.code_do(req, res, next);
+    });
 
     //register form route
     let register = require('../app/controllers/login_ctrl');
     router.get('/register', function(req, res, next) {
         register.register(req, res, next);
-    });
-
-    //home route
-    let home = require('../app/controllers/home_ctrl');
-    router.get('/home', function(req, res, next) {
-        home.index(req, res, next);
     });
 
 };
